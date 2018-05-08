f99dee06082a07e39e20bfb3d49907ec42e59448
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-08 20:52:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,12 +10,12 @@ class App < Sinatra::Base
     @coach = params["coach"]
     @pg = params["pg"]
     @sg = params["sg"]
-    <input type="text" name="coach"></p>
-    <p>Point Guard: <input type="text" name="pg"></p>
-    <p>Shooting Guard: <input type="text" name="sg"></p>
-    <p>Small Forward: <input type="text" name="sf"></p>
-    <p>Power Forward: <input type="text" name="pf"></p>
-    <p>Centre: <input type="text" name="c"></p>
+    # <input type="text" name="coach"></p>
+    # <p>Point Guard: <input type="text" name="pg"></p>
+    # <p>Shooting Guard: <input type="text" name="sg"></p>
+    # <p>Small Forward: <input type="text" name="sf"></p>
+    # <p>Power Forward: <input type="text" name="pf"></p>
+    # <p>Centre: <input type="text" name="c"></p>
   end
 
   get '/team' do
