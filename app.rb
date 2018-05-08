15fadb97ec99c4e751d920f2876f65ca1bc5eaca
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-08 21:59:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,15 +6,14 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    params.each do {|k,v| @k = v}
-    # @name = params["name"]
-    # @coach = params["coach"]
-    # @guard = params["pg"]
-    # @sg = params["sg"]
-    # @sf = params["sf"]
-    # @pf = params["pf"]
-    # <p>Small Forward: <input type="text" name="sf"></p>
-    # <p>Power Forward: <input type="text" name="pf"></p>
+    # params.each do {|k,v| @k = v}
+    @name = params["name"]
+    @coach = params["coach"]
+    @guard = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @c = params["c"]
     # <p>Centre: <input type="text" name="c"></p>
 
     erb :team
