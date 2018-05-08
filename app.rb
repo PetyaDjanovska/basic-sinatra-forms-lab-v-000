7b512c7b3195ad492850055ddca5cd3997d553f7
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-08 20:52:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,6 +7,15 @@ class App < Sinatra::Base
 
   post '/team' do
     @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    <input type="text" name="coach"></p>
+    <p>Point Guard: <input type="text" name="pg"></p>
+    <p>Shooting Guard: <input type="text" name="sg"></p>
+    <p>Small Forward: <input type="text" name="sf"></p>
+    <p>Power Forward: <input type="text" name="pf"></p>
+    <p>Centre: <input type="text" name="c"></p>
   end
 
   get '/team' do
